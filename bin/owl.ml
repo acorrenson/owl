6f1824fe89065a8a62c773540283764d5fda60db
Arthur Correnson <arthur.correnson@gmail.com>
2020-04-16 18:04:33 +0200

    Add solve_all

--- bin/owl.ml
+++ bin/owl.ml
@@ -8,12 +8,21 @@ let display_sol qry sol =
     print_endline "no rules or knowledge matching this query"
   | Some u -> str_of_term (apply_subst u qry) |> print_endline
 
+let display_sol2 qry sols =
+  match sols with
+  | [] -> print_endline "no rules or knowledge matching this query"
+  | _ ->
+    List.iter
+      (fun u -> str_of_term (apply_subst u qry) |> print_endline) sols
+
 let repl db =
   try while true do
       print_string "λ "; flush stdout;
       let inp = read_line () in
       match parse_command inp with
-      | Some qry -> solve qry db |> display_sol qry
+      | Some qry ->
+        (* solve qry db |> display_sol qry *)
+        solve_all qry db |> display_sol2 qry
       | None -> print_endline "!! invalid query !!"
     done
   with End_of_file -> print_endline "Bye !"; exit 0
--- lib/core/solver.ml
+++ lib/core/solver.ml
@@ -1,14 +1,5 @@
 open Unification
 
-let rec query qry db =
-  match db with
-  | [] -> []
-  | r::tail ->
-    match unify [qry, r] with
-    | None -> query qry tail
-    | Some s ->
-      [s] @ (query qry tail)
-
 type rule =
   | Rule of (int ref) * term * term list
   | Know of (int ref) * term
@@ -52,41 +43,6 @@ let str_of_substll ll =
   List.fold_left (fun a t -> a ^ (str_of_substl t) ^ " ") " " ll
 
 
-(* let rec solve_one qry rules =
-   let open List in
-   match rules with
-   | [] -> []
-   | (Rule (n, t, tl) as r)::tail ->
-    if !n >= 500 then [] else
-      begin
-        match unify [qry, (rename !n t)] with
-        | None -> solve_one qry (tail @ [r])
-        | Some s ->
-          incr n;
-          let sl1 = solve (map (apply_subst s) (map (rename !n) tl)) (tail @ [r]) in
-          let sl2 = solve_one qry (tail @ [r]) in
-          (List.map (fun x -> x @ s) sl1) @ sl2
-      end
-   | (Know (n, t) as r)::tail ->
-    if !n >= 500 then [] else
-      begin
-        match unify [qry, (rename !n t)] with
-        | None -> solve_one qry (tail @ [r])
-        | Some s ->
-          incr n;
-          s::(solve_one qry (tail @ [r]))
-      end
-
-   and solve qryl rules =
-   match qryl with
-   | [] ->
-    [[]]
-   | q::ql ->
-    let open List in
-    let sols = solve_one q rules in
-    map (fun s -> map (fun l -> l @ s) (solve (map (apply_subst s) ql) rules)) sols
-    |> List.concat *)
-
 let solve qry db =
   let rec step qry rules =
     match rules with
@@ -96,14 +52,12 @@ let solve qry db =
       let t2' = rename !n t2 in
       begin
         match unify [qry, t1'] with
+        | None -> step qry tail
         | Some u ->
           incr n;
-          begin
-            match step (apply_subst u t2') db with
-            | Some u1 -> Some (compose u u1)
-            | None -> None
-          end
-        | None -> step qry tail
+          match step (apply_subst u t2') db with
+          | Some u1 -> Some (compose u u1)
+          | None -> None
       end
     | Know (n, t)::tail ->
       let t' = rename !n t in
@@ -116,3 +70,39 @@ let solve qry db =
     | _ -> assert false
   in
   step qry db
+
+
+let solve_all qry db =
+  let open List in
+
+  let rec qeval frames qry =
+    map (find_rules qry) frames
+    |> flatten
+
+  and find_rules qry frame =
+    map (check_rule qry frame) db
+    |> flatten
+
+  and check_rule qry frame rule =
+    match rule with
+    | Know (n, t) ->
+      incr n;
+      (match unify [apply_subst frame qry, rename !n t] with
+       | None -> []
+       | Some u -> [compose frame u])
+    | Rule (n, t, lt) ->
+      incr n;
+      (match unify [apply_subst frame qry, rename !n t] with
+       | None -> []
+       | Some u ->
+         let conj = map (rename !n) lt |> map (apply_subst u) in
+         qeval_conjoin [compose u frame] conj)
+
+  and qeval_conjoin frames qrys =
+    match qrys with
+    | [] -> frames
+    | q::ql -> qeval_conjoin (qeval frames q) ql
+  in
+
+  qeval [[]] qry
+
